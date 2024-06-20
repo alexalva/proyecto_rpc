cf5f908d7b98ff8e3798230aef1725013cbaf1da
alexalva <alex250494@gmail.com>
2024-06-19 19:52:28 -0600

    ReadMe

--- ReadMe.md
+++ ReadMe.md
@@ -1,25 +1,40 @@
 # Proyecto RPC
  Proyecto RPC de Cómputo Distribuido
 
-## Configuración del Servidor
-Ejecuta el Servidor en una Máquina:
+Estructura y Componentes Principales
+Servidor RPyC: Utiliza RPyC para establecer una comunicación entre los clientes y el servidor. Facilita la ejecución de operaciones remota y sincronizada entre varias réplicas del servidor.
+StateMachine: Una máquina de estados que mantiene el estado actual del servidor. Realiza operaciones como set , add , mult , y get sobre un diccionario replicado. Lógica de Consenso: Implementa una mezcla de los algoritmos de Lamport para relojes lógicos y Ricart y Agrawala para el manejo de exclusiones mutuas en un entorno distribuido.
+Solución de Consenso
+1. Reloj de Lamport: Cada réplica mantiene un reloj lógico que se incrementa con cada operación. Este reloj ayuda a establecer un orden total entre todas las operaciones, incluso en presencia de desfases temporales entre servidores.
+2. Solicitudes de Operación: Cuando una réplica quiere realizar una operación, primero envía una solicitud a todas las demás réplicas, incrementando su reloj de Lamport.
+3. Cola de Prioridad: Cada réplica mantiene una cola de prioridades basada en los relojes de Lamport de las solicitudes recibidas, asegurando que las operaciones se realicen en el orden correcto.
+4. Condiciones de Ricart y Agrawala: Para garantizar el acceso exclusivo a los recursos compartidos, cada réplica espera recibir un mensaje de "OK" de todas las otras réplicas antes de proceder con una operación. Esto asegura que solo una réplica puede realizar una operación crítica a la vez.
+5. Proceso de Consumo: Las operaciones se consumen de la cola de prioridades solo cuando se cumplen dos condiciones: la operación tiene el sello de tiempo más bajo, y la réplica ha recibido confirmación de todas las otras réplicas.
+6. Replicación y Actualización: Después de realizar una operación, la réplica envía un mensaje a todas las otras réplicas para informarles de la operación realizada, asegurando que todas las réplicas mantengan un estado consistente.
+Componentes del Sistema Máquina de Estados ( StateMachine )
+Objetivo: Gestionar el estado compartido y la cola de operaciones. Características Principales:
+data : Un diccionario que almacena el estado actual de las variables.
+buffer : Una cola de prioridades que ordena las operaciones por su timestamp de Lamport.
+lamport_clock : Un contador que implementa el reloj de Lamport para ordenar eventos en el sistema distribuido.
+Servidor RPyC ( MyService )
+Objetivo: Facilitar la comunicación entre las réplicas y manejar solicitudes de clientes. Funcionalidades:
+Inicialización de sockets para comunicación entre réplicas.
+Procesamiento de mensajes entrantes para solicitudes de acceso, operaciones y replicación de estados.
+Manejo de operaciones CRUD (crear, leer, actualizar, eliminar) en el estado compartido.
+Flujo de Operaciones
+1. Inicialización: Al arrancar, el servidor lee la configuración de un archivo y se inicia, escuchando tanto las solicitudes de RPyC como las conexiones de réplicas.
+2. Replicación y Consenso:
+Cuando una réplica recibe una operación, incrementa su lamport_clock y difunde la operación a todas las réplicas, incluyendo su timestamp de Lamport.
+Utiliza el algoritmo de Ricart y Agrawala para asegurar el acceso exclusivo al log de operaciones, garantizando que las operaciones se serialicen de forma única en todas las réplicas.
+Las operaciones se mantienen en una cola de prioridades hasta que se cumplen las condiciones para su ejecución.
+3. Ejecución de Operaciones:
+Una vez concedido el acceso, la operación se ejecuta y se registra en el log, asegurando la consistencia entre todas las réplicas.
+      Implementación Específica
 
-Elige una máquina en tu LAN para actuar como servidor.
-Asegúrate de que el firewall de la máquina del servidor permita conexiones entrantes en el puerto que estás utilizando (por ejemplo, el puerto 18812).
-
-### Obtén la Dirección IP del Servidor:
-
-Necesitas la dirección IP de la máquina del servidor dentro de tu LAN. Puedes obtenerla generalmente ejecutando ipconfig (en Windows) o ifconfig (en sistemas basados en Unix) en una terminal.
-Asegúrate de que esta IP sea accesible desde otras máquinas en tu LAN.
-
-## Configuración del Cliente
-Ejecuta el Cliente en una Máquina Diferente:
-
-En la máquina que actuará como cliente, asegúrate de que el script del cliente (client.py) esté disponible.
-Modifica el Código del Cliente para Conectarse al Servidor:
-
-En client.py, en lugar de conectarte a "localhost", debes conectarte a la dirección IP del servidor. Por ejemplo:
-python
-Copy code
-conn = rpyc.connect("192.168.1.10", 18812)  # Reemplaza con la IP real del servidor
-Asegúrate de que el puerto (18812 en tu caso) sea el mismo que el servidor está escuchando.
+Manejo de Reloj de Lamport: Cada evento incrementa el reloj de Lamport, y cada mensaje entre réplicas incluye el timestamp de Lamport para ajustar el reloj y mantener un orden causal.
+Cola de Prioridades de Solicitudes: Las operaciones entrantes se encolan en un orden determinado por el timestamp de Lamport, asegurando un ordenamiento global de las operaciones.
+Consenso para Ejecución de Operaciones: Antes de ejecutar una operación del buffer, se solicita el consenso de todas las réplicas utilizando un mecanismo similar al algoritmo de Ricart y Agrawala, contando las respuestas recibidas para decidir si proceder.
+Consideraciones de Diseño
+Consistencia de Datos: A través de la replicación y el consenso, el sistema asegura que todas las réplicas mantengan un estado consistente, ejecutando las mismas operaciones en el mismo orden.
+Conclusión
+Este sistema de replicación distribuida ofrece una solución robusta y eficiente para gestionar el estado compartido en entornos distribuidos, aprovechando los algoritmos de Lamport y Ricart y Agrawala para garantizar la consistencia y la exclusión mutua distribuida. Su implementación en Python facilita la integración con diversos servicios, promoviendo la escalabilidad y la confiabilidad en sistemas distribuidos.
--- server.py
+++ server.py
@@ -1,24 +1,335 @@
+# Servidor
 import rpyc
-from StateMachine import StateMachine
+from rpyc.utils.server import ThreadedServer
+from StateMachine4 import StateMachine
+from operation_future import OperationFuture
+import threading
+import time
+import socket
+import json
+import argparse
+
+def leer_configuracion(archivo_config):
+    with open(archivo_config, 'r') as file:
+        lines = [line.strip() for line in file if line.strip() and not line.startswith('#')]
+    # Asume que la primera línea es para el servidor RPyC y la segunda línea es para el servidor de socket de réplica
+    rpyc_address = tuple(lines[0].split(','))  
+    rpyc_address = (rpyc_address[0], int(rpyc_address[1]))
+    socket_address = tuple(lines[1].split(','))  
+    socket_address = (socket_address[0], int(socket_address[1]))
+    # El resto de las líneas son direcciones de réplicas
+    replica_addresses = [tuple(line.split(',')) for line in lines[2:]]  
+    replica_addresses = [(addr[0], int(addr[1])) for addr in replica_addresses]
+    return rpyc_address, socket_address, replica_addresses
+
+
 
 class MyService(rpyc.Service):
-    def __init__(self):
+    def __init__(self, replica_addresses, my_address):
         self.sm = StateMachine()
+        self.replica_addresses = replica_addresses  # Lista de direcciones IP de las réplicas
+        self.my_address = my_address  # Dirección IP y puerto de este servidor
+        # Iniciar un hilo para procesar elementos del buffer continuamente
+        threading.Thread(target=self.process_buffer, daemon=True).start()
+        threading.Thread(target=self.init_socket_server, daemon=True).start()
+
+        # Inicia un nuevo hilo para imprimir los datos continuamente
+        threading.Thread(target=self.print_data_continuously, daemon=True).start()
+
+    def init_socket_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(self.my_address)
+        server_socket.listen()
+        print(f"Escuchando en {self.my_address} para réplicas...")
+        while True:
+            client_socket, addr = server_socket.accept()
+            print(f"Conexión entrante de {addr}")
+            threading.Thread(target=self.handle_client_socket, args=(client_socket,), daemon=True).start()
+    
+    def handle_client_socket(self, client_socket):
+        with client_socket:
+            while True:
+                data = client_socket.recv(1024)  # Tamaño del buffer 1024 bytes
+                if not data:
+                    break  # Conexión cerrada
+                message = json.loads(data.decode('utf-8'))
+                print(message)
+                
+                if message.get('type') == 'request_access':
+                    # Manejar la solicitud de acceso aquí
+                    self.handle_access_request(message, client_socket)  # Decodifica y carga JSON directamente
+
+                elif message.get('type') == 'operation_consumed':
+                    # Asegúrate de que el mensaje es para la próxima operación esperada
+                    next_item = self.sm.peek()  # Utiliza el método peek que discutimos antes
+                    print(f" Operacion consumida en otro servidor intentando consumir")
+                    # if next_item and next_item[0] == message['timestamp']:
+                    # Consume la operación del buffer
+                    consumed_operation = self.sm.consume()
+                    if consumed_operation:
+                        timestamp, operation, key, value, future = consumed_operation
+
+                        # Procesa la operación de manera similar a cómo lo hace process_buffer
+                        try:
+                            if operation == 'set':
+                                result = self.sm.set(key, value)
+                            elif operation == 'add':
+                                result = self.sm.add(key, value)
+                            elif operation == 'mult':
+                                result = self.sm.mult(key, value)
+                            elif operation == 'get':
+                                result = self.sm.get(key)
+                            else:
+                                raise ValueError("Operación desconocida")
+
+                            # if future is not None:
+                            #     future.set_result(result)
+
+                            print(f"Operación {operation} consumida y procesada con éxito.")
+
+                        except Exception as e:
+                            # if future is not None:
+                            #     future.set_exception(e)
+                            print(f"Error al procesar operación {operation}: {e}")
+
+                    # Procesa la operación como se requiera
+                    print(f"Operación consumida aqui mismo como servidor réplica: {consumed_operation}")
+                    # else:
+                    #     print("Inconsistencia detectada o mensaje de consumo adelantado recibido.")
+
+                elif 'operation' in message:
+                # Actualiza el reloj de Lamport con el sello de tiempo recibido
+                    received_timestamp = message.get("timestamp", 0)  # Asume un valor por defecto si no se proporciona
+                    self.sm.update_clock(received_timestamp)
+
+
+                    operation = message['operation']['action']
+                    key = message['operation']['key']
+                    value = message['operation']['value']
+
+                    # Incrementa el reloj de Lamport antes de encolar la operación
+                    timestamp = self.sm.increment_clock()
+
+                    self.sm.produce(operation, key, value, timestamp)  # Encola la operación
+                    print(f"Operación encolada. Tamaño del buffer ahora: {self.sm.buffer.qsize()}")
+                else:
+                    print("Mensaje no reconocido o mal formado.")
+
+
+    def handle_access_request(self, message, client_socket):
+        # Extraer el sello de tiempo de Lamport y la dirección del servidor solicitante del mensaje
+        request_timestamp = message['timestamp']
+        requesting_server = message['server_address']
+
+        print(f"Recibida solicitud de acceso de {requesting_server} con timestamp {request_timestamp}.")
+
+
+         # Utiliza peek para obtener el sello de tiempo del próximo item sin removerlo
+        next_item = self.sm.peek()
+        next_timestamp = next_item[0] if next_item else float(0)
+
+        # Actualizar el reloj de Lamport basado en el sello de tiempo recibido
+        self.sm.update_clock(request_timestamp)
+        print(f"Reloj de Lamport actualizado a {self.sm.lamport_clock} tras recibir solicitud.")
+
+        # Decidir si conceder o no el acceso
+        # Este ejemplo concede el acceso de manera predeterminada, pero deberías implementar tu lógica
+        # específica de control de acceso aquí. Esto podría incluir comprobaciones como:
+        # - Si el servidor ya está realizando una operación crítica
+        # - Si hay operaciones pendientes con sellos de tiempo menores, etc.
+        access_granted = request_timestamp <= next_timestamp # Esta línea es solo un marcador de posición
+
+        print(f"Solicitud Timestamp: {request_timestamp}")
+        print(f"El timestamp de mi buffer: {next_timestamp}")
+
+        if access_granted:
+            print("Acceso concedido basado en la comparación de timestamps.")
+        else:
+            print(f"Acceso denegado hacia {requesting_server}. La operación entrante con tiempo {request_timestamp} no es la próxima esperada basada en el timestamp.")
+
+        # Construir el mensaje de respuesta
+        response_message = json.dumps({
+            'type': 'access_response',
+            'approval': access_granted,
+            'timestamp': self.sm.increment_clock(),  # Incrementar el reloj de Lamport para este evento
+            'responding_server': self.my_address
+        })
+
+        # Enviar la respuesta al servidor solicitante
+        try:
+            client_socket.sendall(response_message.encode('utf-8'))
+        except Exception as e:
+            print(f"Error al enviar la respuesta de acceso a {requesting_server}: {e}")
+
+
+    def replicate_to_peers(self, operation, key, value):
+        # Obtén el sello de tiempo de Lamport actual
+        timestamp = self.sm.increment_clock()
+
+        # Construye el mensaje incluyendo el sello de tiempo de Lamport
+        message = json.dumps({
+            "operation": {
+                "action": operation,
+                "key": key,
+                "value": value
+            },
+            "timestamp": timestamp  # Incluye el sello de tiempo de Lamport aquí
+        })
+        for address in self.replica_addresses:
+            try:
+                with socket.create_connection(address, timeout=10) as sock:
+                    sock.sendall(message.encode('utf-8'))
+            except Exception as e:
+                print(f"Error replicando a {address}: {e}")
 
     def exposed_read(self, key):
+        # Esta operación puede permanecer igual, ya que get no necesita interactuar con el buffer
         return self.sm.get(key)
 
     def exposed_update(self, key, value, operation):
-        if operation == 'set':
-            return self.sm.set(key, value)
-        elif operation == 'add':
-            return self.sm.add(key, value)
-        elif operation == 'mult':
-            return self.sm.mult(key, value)
+        result = self.sm.produce(operation, key, value)
+        if result:
+            self.replicate_to_peers(operation, key, value)
+        return result
+
+    def process_buffer(self):
+        # Hilo que consume operaciones del buffer y las aplica
+        while True:
+            print("Intentando Consumir...")
+            if self.request_access_to_consume():
+                op = self.sm.consume()
+                if op is not None:
+                    # Asegúrate de que op tenga la longitud esperada antes de desempaquetar
+                    if len(op) == 5:
+                        timestamp, operation, key, value, future = op
+                        print(f"Consumiendo operación: {operation} {key}={value} con timestamp {timestamp}")
+                        # Simular un tiempo de procesamiento
+                        time.sleep(10)  # Espera de 2 segundos para ilustrar el consumo lento
+
+                        try:
+                            # Realiza la operación y establece el resultado en el futuro
+                            if operation == 'set':
+                                result = self.sm.set(key, value)
+                            elif operation == 'add':
+                                result = self.sm.add(key, value)
+                            elif operation == 'mult':
+                                result = self.sm.mult(key, value)
+                            elif operation == 'get':
+                                result = self.sm.get(key)
+                            else:
+                                raise ValueError("Operación desconocida")
+
+                            # Si la operación tiene un futuro asociado, establece el resultado
+                            if future is not None:
+                                future.set_result(result)
+
+                            self.confirm_operation_consumption(timestamp)
+
+                        except Exception as e:
+                            # Si ocurre un error durante la operación, establece la excepción en el futuro
+                            if future is not None:
+                                future.set_exception(e)
+
+                    else:
+                        print("Operación consumida no tiene el formato esperado.")
+            else:
+                print("Esperando aprobación para consumir del buffer...")  # Manejar el caso en el que consume devuelva None (por ejemplo, esperando un poco antes de reintentar)
+            time.sleep(3)
+
+    def request_access_to_consume(self):
+        # Incrementar el reloj de Lamport para esta acción
+        my_timestamp = self.sm.increment_clock()
+        next_item = self.sm.peek()
+        next_timestamp = next_item[0] if next_item else float(0)
+
+        # Construir el mensaje de solicitud
+        request_message = json.dumps({
+            'type': 'request_access',
+            'timestamp': next_timestamp,
+            'server_address': self.my_address
+        })
+
+        responses = []
+
+        # Enviar solicitud de acceso a todos los servidores réplica
+        for address in self.replica_addresses:
+            try:
+                # Abrir conexión con el servidor réplica
+                with socket.create_connection(address, timeout=5) as sock:
+                    # Enviar solicitud
+                    sock.sendall(request_message.encode('utf-8'))
+                    
+                    # Recibir respuesta
+                    response = sock.recv(1024).decode('utf-8')
+                    response_data = json.loads(response)
+                    
+                    # Verificar si la respuesta es de aprobación
+                    if response_data.get('type') == 'access_response' and response_data.get('approval'):
+                        responses.append(True)
+                    else:
+                        responses.append(False)
+            except Exception as e:
+                print(f"Error al enviar solicitud de acceso a {address}: {e}")
+                responses.append(False)
+
+        # Verificar si todas las respuestas son afirmativas
+        if all(responses):
+            print("Acceso al buffer concedido por todos los servidores.")
+            return True
         else:
+            print("Acceso al buffer denegado por al menos un servidor.")
             return False
+        
+    def confirm_operation_consumption(self, operation_timestamp):
+        confirmation_message = json.dumps({
+            'type': 'operation_consumed',
+            'timestamp': operation_timestamp,
+            'server_address': self.my_address
+        })
+        # Envía este mensaje a todos los servidores réplica
+        for address in self.replica_addresses:
+            try:
+                with socket.create_connection(address, timeout=10) as sock:
+                    sock.sendall(confirmation_message.encode('utf-8'))
+            except Exception as e:
+                print(f"Error enviando confirmación de consumo a {address}: {e}")
+    
+    def exposed_get(self, key):
+        """
+        Accede directamente al estado para obtener el valor de una clave.
+        Este método no encola la solicitud y devuelve el valor inmediatamente.
+        """
+        return self.sm.get(key)
+
+    # def exposed_get(self, key):
+    #     future = OperationFuture()
+    #     self.sm.produce('get', key, None, future)
+    #     return future.get_result()
+    
+    def print_data_continuously(self):
+        while True:
+            with self.sm.semaphore:  # Asegurar acceso exclusivo al diccionario 'data'
+                print("Valores actuales en el diccionario 'data':")
+                for key in ['clave1', 'clave2', 'clave3']:
+                    value = self.sm.data.get(key, 'No definido')
+                    print(f"  {key}: {value}")
+            time.sleep(5)  # Espera 5 segundos antes de imprimir nuevamente
+
+
+
 
 if __name__ == "__main__":
-    from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(MyService, port=18812)
+    parser = argparse.ArgumentParser(description='Servidor RPyC con configuración de réplicas.')
+    parser.add_argument('config_file', type=str, help='Archivo de configuración del servidor.')
+    args = parser.parse_args()
+
+    # Leer configuración del servidor
+    rpyc_address, socket_address, replica_addresses = leer_configuracion(args.config_file)
+    
+    # Crear instancia del servicio con la configuración leída
+    service = MyService(replica_addresses, socket_address)
+    
+    # Iniciar el servidor RPyC con la instancia de servicio y el puerto correcto
+    t = ThreadedServer(service, port=rpyc_address[1])
     t.start()
+
